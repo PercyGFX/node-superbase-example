6071f0f9c4cfcd742bab6991c12495ae31426f21
PercyGFX <isurangabtk@gmail.com>
2024-05-21 00:54:44 +0530

    Update supabaseController.ts

--- src/controllers/supabaseController.ts
+++ src/controllers/supabaseController.ts
@@ -1,16 +1,17 @@
-import { Response, Request } from "express"
+import { Response, Request } from "express";
 import supabase from "../utils/superbase.js";
 
 export const getusers = async (req: Request, res: Response) => {
+  const userId = 1;
 
-    const userId = 1
+  const { data, error } = await supabase
+    .from("posts")
+    .select()
+    .eq("user_id", userId);
 
- const { data, error } = await supabase
-   .from("posts")
-   .select()
-   .eq("user_id", userId);
+  console.log(data);
 
- console.log(data)
-
-    res.json(data);
+  res.json(data);
 };
+
+// jhhjhjg
